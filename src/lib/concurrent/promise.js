4449e0599b6ed9aa2e075354ce1decb725d51249
Didier Plaindoux <d.plaindoux@free.fr>
2017-08-30 18:27:09 +0200

    Revert

--- src/lib/concurrent/promise.js
+++ src/lib/concurrent/promise.js
@@ -1,6 +1,6 @@
 /*
  * promiseMe.js
- * https://github.com/d-plaindoux/magnet
+ * https://github.com/d-plaindoux/promiseMe.js
  *
  * Copyright (c) 2017 Didier Plaindoux
  * Licensed under the LGPL2 license.
--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,10 @@
 /*
+<<<<<<< HEAD
  * fun.js
+=======
+ * promiseMe.js
+ * https://github.com/d-plaindoux/promiseMe.js
+>>>>>>> Revert
  *
  * Copyright (c) 2017 Didier Plaindoux
  * Licensed under the LGPL2 license.
